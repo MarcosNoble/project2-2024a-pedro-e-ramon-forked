43c64727d3184ea0c758f65736a2a8b90fccac3c
Ramon <ramonizidoro22@gmail.com>
2024-04-18 11:16:58 -0300

    2 scripts de testes de api e node

--- script.js
+++ script.js
@@ -4,3 +4,33 @@ function toggleDropdown() {
   }
   
   
+// Define o endpoint da API
+const url = "https://quizapi.io/api/v1/categories";
+// Define o parâmetro da chave da API
+const apiKey = "iXaI9iMzp8MnpIwVW1tUpnaEuw0iRDhhvWXBToVU";
+
+// Seleciona o elemento UL onde os dados serão exibidos
+const categoriasLista = document.getElementById('categorias-lista');
+
+// Faz a solicitação GET para o endpoint da API usando fetch
+fetch(`${url}?apiKey=${apiKey}`)
+  .then(response => {
+    // Verifica se a resposta foi bem-sucedida (código de status 200)
+    if (!response.ok) {
+      throw new Error('Erro ao fazer a solicitação: ' + response.status);
+    }
+    // Converte a resposta para JSON
+    return response.json();
+  })
+  .then(data => {
+    // Para cada categoria, cria um item de lista e adiciona ao elemento UL
+    data.forEach(categoria => {
+      const listItem = document.createElement('li');
+      listItem.textContent = categoria.name;
+      categoriasLista.appendChild(listItem);
+    });
+  })
+  .catch(error => {
+    // Manipula erros
+    console.error('Houve um erro:', error);
+  });
